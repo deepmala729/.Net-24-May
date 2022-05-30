8e43cfe9d7e955f2bfd27c5c80eec32a68c78d65
Deepmala Gupta <deepmala729@gmail.com>
2022-05-30 09:56:47 +0530

    first commit

---
+++ ConsoleApp2/ConsoleApp2/Class7.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp2
+{
+    
+    class A
+    {
+        protected int rn;
+
+    }
+    //Derived Class
+    class B : A
+    {
+        public void Print()
+        {
+            Console.WriteLine(rn);
+        }
+    }
+    class Class7
+    {
+        public static void Main9()
+        {
+
+        }
+    }
+}
